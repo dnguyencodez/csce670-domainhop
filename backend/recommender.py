be84529528c7d78050252ae79df108aa96302e61
Dylan Nguyen <dylan.nguyen39@gmail.com>
2023-04-28 01:58:38 -0500

    minor fixups to recommender.py

--- backend/recommender.py
+++ backend/recommender.py
@@ -2,7 +2,6 @@ import pandas as pd
 import numpy as np
 from sklearn.decomposition import NMF
 
-# Define the genre mapping dictionary
 genre_mapping = {
     'Adventure': ['Adventure', 'Role-Playing'],
     'Animation': ['Platform', 'Adventure'],
